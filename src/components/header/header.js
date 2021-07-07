1aa54930840756683869903dc822a0bb69796ab2
David Hevesi <davidhevesi2008@gmail.com>
2021-07-07 06:56:37 -0400

    Create new header && header options components.

---
+++ src/components/header/header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import "./header.css";
+import HeaderOption from "./headerOption";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" placeholder="search" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://lh3.googleusercontent.com/ogw/ADea4I4W7SuaqeBFlSStcOKOmX9ioPXe2t93hT2K9FgRMQ=s32-c-mo"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default header;
